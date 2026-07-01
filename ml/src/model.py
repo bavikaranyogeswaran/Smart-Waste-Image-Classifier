8e4562646d22f97cba4ba276a2b15985a7c37e82
Bavikaran Yogeswaran <bavikaranyogeswaran@gmail.com>
2026-07-01 18:59:52 +0530

    Replaced ResNet18 fc layer with custom classification head

--- ml/src/model.py
+++ ml/src/model.py
@@ -13,4 +13,7 @@ def build_model(num_classes):
     for param in model.parameters():
         param.requires_grad = False
 
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, num_classes)
+
     return model
