4dac67ec67e5146829366ac0f41cedab50b2588e
Bavikaran Yogeswaran <bavikaranyogeswaran@gmail.com>
2026-07-01 18:27:20 +0530

    Add train_transforms to dataset.py

--- ml/src/dataset.py
+++ ml/src/dataset.py
@@ -2,3 +2,16 @@
 
 Placeholder created in Step 1. Implemented in Step 5.
 """
+from torchvision import transforms
+
+train_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
